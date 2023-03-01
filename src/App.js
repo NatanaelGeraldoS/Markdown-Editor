c58ed86e9aa9aa5bd0eac5674869df672dacdda0
NatanaelGeraldoS <geradonatanael84@gmail.com>
2023-03-01 10:23:30 +0700

    CHANGE : Display Styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faDesktop, faUpRightAndDownLeftFromCenter, faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons'
-
+// Markdown
 import Prism from "prismjs"
 import "prismjs/themes/prism-okaidia.min.css"
 import { convertMd } from "./util/convert-md"
+// PDF
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
 class App extends Component {
   constructor() {
@@ -43,12 +46,35 @@ class App extends Component {
   MinSize() {
     this.setState({ editorFullScreen: false, viewerFullScreen: false })
   }
+  DownloadPDF() {
+    const Input = document.getElementById("ViewerData");
+    html2canvas(Input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.save('Download.pdf');
+      })
+
+    fetch('SamplePDF.pdf').then(response => {
+      response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = 'SamplePDF.pdf';
+        alink.click();
+      })
+    })
+  }
   render() {
 
 
     return (
       <div>
         <Header />
+        <button className='btn btn-btn-primary' onClick={this.DownloadPDF}>Download PDF</button>
         <div className=' row mx-auto justify-content-evenly mt-5 pb-5'>
           <div id='editor-side' className={`card p-0 mb-5 ${this.state.editorFullScreen ? 'col-12' : this.state.viewerFullScreen ? 'd-none' : 'col-md-5'}`} >
             <HeaderCard FullScreen={this.state.editorFullScreen} MinSize={this.MinSize} MaxSize={this.MaxSize} Value={"Editor"} Icon={faPenToSquare} />
@@ -56,7 +82,7 @@ class App extends Component {
           </div>
           <div id='viewer-side' className={`card p-0 mb-5 ${this.state.viewerFullScreen ? 'col-12' : this.state.editorFullScreen ? 'd-none' : 'col-md-5'}`}>
             <HeaderCard FullScreen={this.state.viewerFullScreen} MinSize={this.MinSize} MaxSize={this.MaxSize} Value={"Viewer"} Icon={faDesktop} />
-            <div dangerouslySetInnerHTML={{ __html: this.state.text }} className="p-2" />
+            <div dangerouslySetInnerHTML={{ __html: this.state.text }} className="p-2" id='ViewerData' />
           </div>
         </div>
       </div>
@@ -82,12 +108,14 @@ const HeaderCard = (props) => {
       </div>
       <div>
         <button onClick={props.FullScreen ? props.MinSize : props.MaxSize} value={props.Value} className='d-md-block d-none'>
-          {props.FullScreen
-            ?
-            <FontAwesomeIcon icon={faDownLeftAndUpRightToCenter} />
-            :
-            <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
-          }
+          <span style={{ color: 'black' }}>
+            {props.FullScreen
+              ?
+              <FontAwesomeIcon icon={faDownLeftAndUpRightToCenter} />
+              :
+              <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
+            }
+          </span>
         </button>
 
       </div>
